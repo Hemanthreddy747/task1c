82bd10da5cbd0d230cf9cbcc6b0a130fabece409
Hemanth747 <118827928+Hemanthreddy747@users.noreply.github.com>
2023-04-27 21:16:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,138 +1,23 @@
 
-document.getElementById("btn1").style.background = '#000000';
-document.getElementById("btn1").style.color = 'white';
-
-document.getElementById("btn1").onclick = function () {
-  document.getElementById("data").innerHTML = "We offer same day delivery on gift basket orders placed before 11 A.M. PST. We deliver to Los Angeles. We deliver to: Santa Fe Springs, Norwalk, La Mirada, Whittier, Cerritos, Downey, Artesia, Bellflower, Pico Rivera, Buena Park, La Palma, Lakewood, La Habra, Bell, Fullerton, Paramount, Hawaiian Gardens, Montebello, Hacienda Heights, Long Beach, Bell Gardens, Cypress, Los Angeles, South Gate, Anaheim, Los Alamitos, Lynwood, South El Monte, Maywood, Compton, La Puente, Monterey Park, Stanton, Brea, Huntington Park, Rowland Heights, Rosemead, Garden Grove, El Monte, Signal Hill, Carson, San Gabriel, West Covina, Seal Beach, Alhambra, Westminster, Placentia, City Of Industry, Temple City, Walnut, Baldwin Park, Midway City, Gardena, Orange, Surfside, Atwood, Huntington Beach, Arcadia, San Marino, Sunset Beach, South Pasadena, Diamond Bar, Covina, Santa Ana, Torrance, Yorba Linda, Wilmington, Pasadena, Duarte, Inglewood, Monrovia, Fountain Valley, Villa Park, Harbor City, Sierra Madre, Hawthorne, Azusa, Lawndale, Lomita, San Pedro, Glendale, Pomona, Glendora, Redondo Beach, Tustin, Altadena, La Crescenta, Chino Hills, Costa Mesa, San Dimas, Manhattan Beach, El Segundo, Hermosa Beach, Culver City, Irvine, Rancho Palos Verdes, Palos Verdes Peninsula, North Hills, Playa Vista, Beverly Hills, Mount Wilson, La Canada Flintridge, Newport Beach, Montrose, West Hollywood, Playa Del Rey, Aliso Viejo, Marina Del Rey, Burbank, Verdugo City, Universal City, Montclair, Chino, La Verne, Venice, North Hollywood, Claremont, East Irvine, Studio City, Santa Monica, Corona Del Mar, Toluca Lake, Valley Village, Ontario, Corona, Pacoima, Upland, Newport Coast, Tujunga, Sherman Oaks, Foothill Ranch, Sun Valley, Valyermo, Sunland, Van Nuys, Norco, Silverado, Guasti, Laguna Woods, Lake Forest, Rancho Cucamonga, Encino";
-  document.getElementById("dataheading").innerHTML = "LOS ANGELES SAME DAY DELIVERY";
-
-  document.getElementById("btn1").style.background = '#000000';
-  document.getElementById("btn1").style.color = 'white';
-  document.getElementById("btn2").style.background = 'white';
-  document.getElementById("btn2").style.color = '#000000';
-  document.getElementById("btn3").style.background = 'white';
-  document.getElementById("btn3").style.color = '#000000';
-  document.getElementById("btn4").style.background = 'white';
-  document.getElementById("btn4").style.color = '#000000';
 
 
-}
+const scrollText = document.getElementById('trantext');
+let lastScrollPosition = window.pageYOffset;
+let scale = 1;
+window.addEventListener('scroll', () => {
+  const currentScrollPosition = window.pageYOffset;
 
-document.getElementById("btn2").onclick = function () {
-  document.getElementById("data").innerHTML = "We offer next day delivery on gift basket orders placed before 2 P.M. EST. We deliver to Chicago. We deliver to: Aurora, Joliet, Naperville, Rockford, Elgin, Springfield, Peoria, Champaign, Waukegan, Cicero, Bloomington, Evanston, Schaumburg, Arlington Heights, Bolingbrook, Decatur, Skokie, Palatine, Des Plaines, Orland Park, Oak Lawn, Berwyn, Mount Prospect, Tinley Park, Normal, Oak Park, Wheaton, Hoffman Estates, Downers Grove, Glenview, Plainfield, Elmhurst, Lombard, Buffalo Grove, Moline, Belleville, Bartlett, DeKalb, Romeoville, Crystal Lake, Carol Stream, Quincy, Park Ridge, Urbana, Streamwood, Wheeling, Carpentersville, Hanover Park, Rock Island, Addison, Oswego, Calumet City, Northbrook, Woodridge, St. Charles, Glendale Heights, O'Fallon, Elk Grove Village, Mundelein, Pekin, Gurnee, Niles, Highland Park, North Chicago, Algonquin, Galesburg, Lake in the Hills, Burbank, Danville, Glen Ellyn, Lansing, Huntley, Wilmette, Granite City, New Lenox, McHenry, Round Lake Beach, Chicago Heights, Oak Forest, Vernon Hills, Lockport, Batavia, Woodstock, Alton, West Chicago, Edwardsville, Belvidere, Morton Grove, Homer Glen, Zion, Collinsville, Melrose Park, Westmont, Elmwood Park, Kankakee, South Elgin, Lisle, Rolling Meadows, Freeport, Loves Park, Maywood, Machesney Park, Roselle, Yorkville, Bloomingdale, East Peoria, Blue Island, Villa Park, Darien, Carbondale, Montgomery, Park Forest, Geneva, East Moline, Grayslake, South Holland, Dolton, Libertyville, Frankfort, Crest Hill, Harvey, Mokena, Lake Zurich, Evergreen Park, Lake Forest, Dee";
-  document.getElementById("dataheading").innerHTML = "CHICAGO NEXT DAY DELIVERY";
-  document.getElementById("btn2").style.background = '#000000';
-  document.getElementById("btn2").style.color = 'white';
-  document.getElementById("btn1").style.background = 'white';
-  document.getElementById("btn1").style.color = '#000000';
-  document.getElementById("btn3").style.background = 'white';
-  document.getElementById("btn3").style.color = '#000000';
-  document.getElementById("btn4").style.background = 'white';
-  document.getElementById("btn4").style.color = '#0000000';
-}
-document.getElementById("btn3").onclick = function () {
-  document.getElementById("data").innerHTML = "We offer next day delivery on gift basket orders placed before 2 P.M. EST. We deliver to New York City. We deliver to: Hempstead town, Brookhaven, Islip, Oyster Bay, Buffalo, North Hempstead, Babylon town, Rochester city, Yonkers, Huntington, Ramapo, Syracuse, Amherst, Smithtown, Albany, Greece, Greenburgh, Cheektowaga, Clarkstown, Colonie town, New Rochelle, Mount Vernon, Tonawanda town, Southampton town, Schenectady, Utica, Clay, Hamburg town, White Plains, Hempstead, Union, Irondequoit, Troy, Rye town, Orangetown, Niagara Falls, Binghamton city, Perinton, Henrietta, Poughkeepsie town, Webster town, Mount Pleasant, West Seneca, Lancaster town, Freeport, Cortlandt, Valley Stream, Ossining town, Penfield, Haverstraw town, Clifton Park, Guilderland, Brighton town, Palm Tree and Kiryas Joel, Riverhead, Yorktown, Bethlehem, Long Beach, Eastchester, Carmel, Manlius town, Salina, Spring Valley, Clarence, Newburgh town, Warwick town, Rome, Poughkeepsie, Ithaca city, Cicero, Mamaroneck town, Port Chester, Wallkill, Rotterdam, Middletown city, North Tonawanda, Pittsford town, Orchard Park town, East Fishkill, Vestal, Glenville, Queensbury, Gates, Chili, Newburgh, Harrison, East Hampton town, Jamestown, Saratoga Springs, Wappinger, Glen Cove, New Windsor, Ossining, Lindenhurst, Auburn, Elmira city, Halfmoon, Rockville Centre, De Witt, Peekskill, Camillus town, Le Ray, Watertown city, Fishkill town, Kingston city, Southold, Niskayuna, Montgomery town, Lysander, Garden City, Onondaga, New Hartford town, Ithaca, Monroe town, Mineola, Grand Island, Somers, Hyde Park, Lockport city, Lockport, Lynbrook, Ogden, Plattsburgh city, Mamaroneck, Lackawanna, Saugerties town, Horseheads town, Blooming Grove, Milton, Owego town, Wheatfield, Amsterdam city, Southeast, New Castle, Cohoes, Scarsdale, Whitestown, Wilton, Malta, Cortland, Bedford, Oswego cit";
-  document.getElementById("dataheading").innerHTML = "NEW YORK CITY NEXT DAY DELIVERY";
-  document.getElementById("btn3").style.background = '#000000';
-  document.getElementById("btn3").style.color = 'white';
-  document.getElementById("btn2").style.background = 'white';
-  document.getElementById("btn2").style.color = '#000000';
-  document.getElementById("btn1").style.background = 'white';
-  document.getElementById("btn1").style.color = '#000000';
-  document.getElementById("btn4").style.background = 'white';
-  document.getElementById("btn4").style.color = '#000000';
-}
-
-document.getElementById("btn4").onclick = function () {
-  document.getElementById("data").innerHTML = "We offer next day delivery on gift basket orders placed before 2 P.M. EST. We deliver to New Jersey. We deliver to:  Newark, Jersey City, Paterson, Lakewood, Elizabeth, Edison, Woodbridge, Toms River, Hamilton township, Trenton, Clifton, Cherry Hill, Brick, Camden, Passaic, Bayonne, East Orange, Franklin township, Old Bridge, Middletown, Gloucester, Union City, Vineland, Piscataway, North Bergen, Irvington, Union township, Jackson, Hoboken, Parsippany-Troy Hills, New Brunswick, Perth Amboy, Plainfield, Wayne, Howell, Bloomfield, West New York, Washington township, East Brunswick, Monroe township, West Orange, Evesham, Egg Harbor, South Brunswick, Bridgewater, Manchester, Hackensack, Mount Laurel, Sayreville, Berkeley, North Brunswick, Linden, Hillsborough, Teaneck, Marlboro, Montclair, Manalapan, Kearny, Winslow, Fort Lee, Atlantic City, Galloway, Monroe, Belleville, Ewing, Pennsauken, Freehold township, Fair Lawn, City of Orange, Lawrence township, Garfield, Long Branch, Deptford, Willingboro, Voorhees, Livingston, Princeton, Westfield, Rahway, East Windsor, Nutley, Stafford, West Windsor, Lacey, Englewood, Mount Olive, Neptune, Bergenfield, Hamilton, Ocean township, Bernards, Millville, Pemberton township, Bridgeton, Randolph, Wall, Paramus, Ridgewood, Lodi, Rockaway township, Cliffside Park, Maplewood, Mahwah, Carteret, Barnegat, Scotch Plains, West Milford, Medford, South Plainfield, Cranford, Burlington township, Plainsboro, Raritan township, Montgomery, Morris, Roxbury, Summit, North Plainfield, West Deptford, Vernon, Roselle, Montville, Lyndhurst, Hillside, Lower, Millburn, Lindenwold, Moorestown, Secaucus, Elmwood Park, Little Egg Harbor, Glassboro, Middle, Pleasant";
-  document.getElementById("dataheading").innerHTML = "NEW JERSEY NEXT DAY DELIVERY";
-  document.getElementById("btn4").style.background = '#000000';
-  document.getElementById("btn4").style.color = 'white';
-  document.getElementById("btn2").style.background = 'white';
-  document.getElementById("btn2").style.color = '#000000';
-  document.getElementById("btn3").style.background = 'white';
-  document.getElementById("btn3").style.color = '#000000';
-  document.getElementById("btn1").style.background = 'white';
-  document.getElementById("btn1").style.color = '#000000';
-}
-
-
-var slider = document.querySelector('.slider');
-var isDown = false;
-var startX;
-var scrollLeft;
-
-slider.addEventListener('mousedown', function (e) {
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', function () {
-  isDown = false;
-});
-
-slider.addEventListener('mouseup', function () {
-  isDown = false;
-});
-
-slider.addEventListener('mousemove', function (e) {
-  if (!isDown) return;
-  e.preventDefault();
-  var x = e.pageX - slider.offsetLeft;
-  var walk = (x - startX) * 3;
-  slider.scrollLeft = scrollLeft - walk;
-});
-
-
-function toggleMenu() {
-  var menu = document.getElementById("dropdown-menu");
-  if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
+  if (currentScrollPosition > lastScrollPosition) {
+    // Scroll down
+    scale = 0;
   } else {
-    menu.classList.add("show");
+    // Scroll up
+    scale = 1;
   }
-}
 
-function toggleMenu() {
-  var menu = document.getElementById("dropdown-menu");
-  var icon = document.querySelector(".icon");
-  if (menu.classList.contains("show")) {
-    menu.classList.remove("show");
-    icon.classList.remove("close");
-    icon.classList.add("open");
-  } else {
-    menu.classList.add("show");
-    icon.classList.remove("open");
-    icon.classList.add("close");
-  }
-}
+  scrollText.style.transform = `scale(${scale})`;
+  lastScrollPosition = currentScrollPosition;
+});
 
 
 
-
-function toggleDropdown(dropdownNum) {
-  var dropdown = document.getElementById("dropdown" + dropdownNum);
-  var icon = document.getElementById("icon" + dropdownNum);
-  if (dropdown.classList.contains("show")) {
-    dropdown.classList.remove("show");
-    icon.classList.remove("rotate");
-  } else {
-    dropdown.classList.add("show");
-    icon.classList.add("rotate");
-  }
-}
-
-// Close all dropdowns if the user clicks outside of them
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        document.getElementById("icon" + (i + 1)).classList.remove("rotate");
-      }
-    }
-  }
- }
